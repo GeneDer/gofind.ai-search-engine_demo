e998d9793773b42de5fe753664aa84c1590e8067
Gene Der Su <e870252314@yahoo.com.tw>
2016-12-01 13:10:53 -0800

    feat: completed the second version of the post page
    Make stuff data into the new template from Jocy.

--- flaskapp.py
+++ flaskapp.py
@@ -9,7 +9,7 @@ from flask import session as login_session
 app = Flask(__name__)
 app.config.from_object(__name__)
 
-MAX_POST = 871
+MAX_POST = 6889
 MAX_PAGE = (MAX_POST - 1)/9 + 1
 
 
@@ -82,32 +82,42 @@ def post(post_id):
         main_image_url = post[0][1]
         source_url = post[0][2]
         tags = post[0][3]
-        Image_name = post[0][4]
-        
-        #query the results based on the post_id
-        results = select_query("""SELECT * 
-                                  FROM results
-                                  WHERE post_id = ?""",
-                               [post_id])
+        image_name = post[0][4]
 
-        result_reorder = []
-        tmp = []
-        #TODO: test out results after database created
-        for i in xrange(len(results)):
-            tmp.append(results[i])
-            if (i+1)%3 == 0:
-                result_reorder.append(tmp)
-                tmp=[]
-        if tmp:
-            result_reorder.append(tmp)
+        #query the segmented_images based on the post_id
+        segmented_images = select_query("""SELECT *
+                                           FROM segmented
+                                           WHERE post_id = ?""",
+                                        [post_id])
+
+        #limit showing at most 3 segmented_images
+        if len(segmented_images) > 3:
+            segmented_images = segmented_images[:3]
+
+        #query results based on each segmented_images
+        results = []
+        min_results_length = 10
+        for i in segmented_images:
+            tmp = select_query("""SELECT * 
+                                  FROM results
+                                  WHERE segmented_id = ?""",
+                               [i[0]])
+            results.append(tmp)
+            if len(tmp) < min_results_length:
+                min_results_length = len(tmp)
+
+        #limit the length of results to be the min_results_length
+        for i in xrange(len(segmented_images)):
+            results[i] = results[i][:min_results_length]
                 
 
-        #TODO: modify the html after its created
+        #create the template filled with data
         return render_template("post.html", main_image_url=main_image_url,
                                source_url=source_url,
                                tags=tags,
-                               Image_name=Image_name,
-                               results=result_reorder)
+                               image_name=image_name,
+                               segmented_images=segmented_images,
+                               results=results)
     elif post_id < 1:
         return redirect(url_for('post', post_id=1))
     else:
