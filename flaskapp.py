b4b67cb574e913d40b344a30ae1549445b788726
Gene Der Su <e870252314@yahoo.com.tw>
2016-12-08 10:37:53 -0800

    feat: adding Jocy's post2 page
    Jocy wants her post2 pages to be online for testing.

--- flaskapp.py
+++ flaskapp.py
@@ -143,7 +143,84 @@ def post(post_id):
         return redirect(url_for('post', post_id=1))
     else:
         return redirect(url_for('post', post_id=MAX_POST))
-    
+
+@app.route('/post2/<int:post_id>')
+def post2(post_id):
+    MAX_POST = select_query("""SELECT MAX(id)
+                           FROM posts""",
+                           [])[0][0]
+    print MAX_POST
+    if post_id >= 1 and post_id <= MAX_POST:
+        back_to_page = (post_id - 1)/9 + 1
+        pervious_post = (post_id - 1) if post_id != 1 else 1
+        next_post = (post_id + 1) if post_id != MAX_POST else MAX_POST
+
+        #query the post based on the post_id
+        post = select_query("""SELECT * 
+                               FROM posts
+                               WHERE id = ?""",
+                            [post_id])
+        main_image_url = post[0][1]
+        source_url = post[0][2]
+        tags = post[0][3]
+        tags2 = tags.replace('#',",")
+        image_name = post[0][4]
+
+        #query the segmented_images based on the post_id
+        segmented_images = select_query("""SELECT *
+                                           FROM segmented
+                                           WHERE post_id = ?""",
+                                        [post_id])
+
+        #limit showing at most 3 segmented_images
+        if len(segmented_images) > 3:
+            segmented_images = segmented_images[:3]
+
+        #query results based on each segmented_images
+        description = []
+        results = []
+        min_results_length = 10
+        for i in segmented_images:
+            tmp = select_query("""SELECT * 
+                                  FROM results
+                                  WHERE segmented_id = ?""",
+                               [i[0]])
+            results.append(tmp)
+            if len(tmp) < min_results_length:
+                min_results_length = len(tmp)
+            for j in tmp:
+                description.append('%s at %s'%(j[5], j[6]))
+
+        #limit the length of results to be the min_results_length
+        for i in xrange(len(segmented_images)):
+            results[i] = results[i][:min_results_length]
+
+        #process the description
+        if len(description) != 0:
+            description = "Shop for " + ", ".join(description) + '.'
+
+        #process tags2
+        if len(tags2) > 1:
+            tags2 = " - Shopping for " + tags2
+        else:
+            tags2 = ''
+
+        #create the template filled with data
+        return render_template("post2.html", id1=post_id - 1,
+                               id2=post_id + 1,
+                               main_image_url=main_image_url,
+                               source_url=source_url,
+                               tags=tags,
+                               image_name=image_name,
+                               segmented_images=segmented_images,
+                               results=results,
+                               description=description,
+                               tags2=tags2)
+    elif post_id < 1:
+        return redirect(url_for('post', post_id=1))
+    else:
+        return redirect(url_for('post', post_id=MAX_POST))
+
 @app.route('/legal')
 def legal():
     return render_template("legal.html")
